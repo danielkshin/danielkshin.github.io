f7d06e9118cd3b57435371b5e16e45a7895be8cb
Daniel Shin <140947149+danielkshin@users.noreply.github.com>
2024-09-07 21:07:41 -0700

    Restructure assets folder

--- script.js
+++ script.js
@@ -4,63 +4,63 @@ const projects = [
         'name': 'Hankel',
         'description': 'A soccer planner.',
         'link': 'https://danielkshin.github.io/Hankel',
-        'image': 'assets/hankel.png',
+        'image': 'hankel',
         'languages': ['html', 'css', 'js'],
     },
     {
         'name': 'colors (remake)',
         'description': 'An original 2D platformer game with a unique mechanic around colors.',
         'link': 'https://danielkshin.github.io/colors',
-        'image': 'assets/colors.png',
+        'image': 'colors',
         'languages': ['js'],
     },
     {
         'name': 'VOTD Lock Screen Widget',
         'description': 'A Scriptable lock screen widget that displays the YouVersion Bible verse of the day.',
         'link': 'https://www.github.com/danielkshin/VOTD-Lock-Screen-Widget',
-        'image': 'assets/widget.png',
+        'image': 'widget',
         'languages': ['js'],
     },
     {
         'name': 'colors (original)',
         'description': 'An original 2D platformer game with a unique mechanic around colors.',
         'link': 'https://danielkshin.github.io/APCSP-Projects/#colors',
-        'image': 'assets/colorsOriginal.png',
+        'image': 'colorsOriginal',
         'languages': ['py'],
     },
     {
         'name': 'whatever this game is called',
         'description': 'An interactive novel game.',
         'link': 'https://danielkshin.github.io/APCSP-Projects/#whatever-this-game-is-called-an-interactive-novel',
-        'image': 'assets/novel.png',
+        'image': 'novel',
         'languages': ['py'],
     },
     {
         'name': 'WORDLE console',
         'description': 'A WORDLE clone faithfully recreated in the console.',
         'link': 'https://danielkshin.github.io/APCSP-Projects/#wordle-console',
-        'image': 'assets/wordle.png',
+        'image': 'wordle',
         'languages': ['py'],
     },
     {
         'name': 'jump!',
         'description': 'A 2D recreation of a 3D mini-game.',
         'link': 'https://danielkshin.github.io/APCSP-Projects/#jump',
-        'image': 'assets/jump.png',
+        'image': 'jump',
         'languages': ['py'],
     },
     {
         'name': 'bounce!',
         'description': 'A blatant Flappy Bird clone. Well, sort of.',
         'link': 'https://danielkshin.github.io/APCSP-Projects/#bounce',
-        'image': 'assets/bounce.png',
+        'image': 'bounce',
         'languages': ['py'],
     },
     {
         'name': 'omok',
         'description': 'A classic two-player game with the goal of forming a five-in-a-row.',
         'link': 'https://danielkshin.github.io/APCSP-Projects/#omok',
-        'image': 'assets/omok.png',
+        'image': 'omok',
         'languages': ['py'],
     },
 ]
@@ -69,12 +69,12 @@ for (const project of projects) {
     projectsDiv.innerHTML +=
         `<a href="${project.link}" target="_blank">
             <div class="card">
-                <img src="${project.image}">
+                <img src="assets/projects/${project.image}.png">
                 <p><b>${project.name}</b><br>
                     ${project.description}
                 </p>
                 <div class="languages">
-                    <img src="assets/${project.languages.join('.png">\n<img src="assets/')}.png">
+                    <img src="assets/languages/${project.languages.join('.png">\n<img src="assets/languages/')}.png">
                 </div>
             </div>
         </a>`;
