a00b3d1516b8ea95a9dec96a98dec975a67911d5
Daniel Shin <140947149+danielkshin@users.noreply.github.com>
2025-03-19 01:54:41 -0700

    Add more info

--- src/Config.tsx
+++ src/Config.tsx
@@ -22,10 +22,15 @@ const config: SectionDetails[] = [
   {
     title: 'a student',
     content: (
-      <p>
-        I am a freshman studying computer science at the Paul G. Allen School of
-        Computer Science and Engineering at the University of Washington.
-      </p>
+      <>
+        <p>September 2024 - Present </p>
+        <br />
+        <p>
+          I am a first-year student studying Computer Science at the Paul G.
+          Allen School of Computer Science & Engineering at the University of
+          Washington.
+        </p>
+      </>
     ),
     id: 'education',
     backgroundColor: '#4b2e83',
@@ -35,7 +40,14 @@ const config: SectionDetails[] = [
   {
     title: 'an intern',
     content: (
-      <p>I am an incoming SDE intern at Amazon for the summer of 2025.</p>
+      <>
+        <p>Starting June 2025</p>
+        <br />
+        <p>
+          I will be joining Amazon as a Software Development Engineer Intern
+          this summer in Seattle!
+        </p>
+      </>
     ),
     id: 'experience',
     backgroundColor: '#232f3e',
@@ -44,11 +56,11 @@ const config: SectionDetails[] = [
   },
   {
     title: 'a dev',
-    content: <p>test</p>,
+    content: <p>I enjoy building random projects. Check them out!</p>,
     id: 'projects',
-    backgroundColor: '#ff0000',
+    backgroundColor: '#f2f2f2',
     textColor: '#121212',
-    titleColor: '#33dd33',
+    titleColor: '#dc7b5a',
   },
 ];
 
