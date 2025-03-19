f79213b80ca718be0a565d5f08f5c731e4c5362f
Daniel Shin <140947149+danielkshin@users.noreply.github.com>
2025-03-19 01:01:35 -0700

    Separate config file

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,7 @@
-import { ReactElement, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { Section } from 'components';
+import { config, SectionDetails } from 'Config';
 import './App.css';
-
-interface Section {
-  title: string;
-  content: ReactElement;
-  id: string;
-  backgroundColor: string;
-  textColor: string;
-  titleColor: string;
-}
-
-const sections: Section[] = [
-  {
-    title: 'daniel',
-    content: <p>test</p>,
-    id: 'about',
-    backgroundColor: '#f2f2f2',
-    textColor: '#121212',
-    titleColor: '#5aa57b',
-  },
-  {
-    title: 'a student',
-    content: (
-      <p>
-        I am a freshman studying computer science at the Paul G. Allen School of
-        Computer Science and Engineering at the University of Washington.
-      </p>
-    ),
-    id: 'education',
-    backgroundColor: '#4b2e83',
-    textColor: '#f2f2f2',
-    titleColor: '#b7a57a',
-  },
-  {
-    title: 'an intern',
-    content: (
-      <p>I am an incoming SDE intern at Amazon for the summer of 2025.</p>
-    ),
-    id: 'experience',
-    backgroundColor: '#232f3e',
-    textColor: '#ff9900',
-    titleColor: '#f2f2f2',
-  },
-  {
-    title: 'a dev',
-    content: <p>test</p>,
-    id: 'projects',
-    backgroundColor: '#ff0000',
-    textColor: '#121212',
-    titleColor: '#33dd33',
-  },
-];
 
 const App = () => {
   const containerRef = useRef<HTMLDivElement>(null);
@@ -70,15 +20,12 @@ const App = () => {
       currentSectionRef.current = newSection;
       container.style.setProperty(
         '--bg-color',
-        sections[newSection].backgroundColor
+        config[newSection].backgroundColor
       );
-      container.style.setProperty(
-        '--text-color',
-        sections[newSection].textColor
-      );
+      container.style.setProperty('--text-color', config[newSection].textColor);
       container.style.setProperty(
         '--title-color',
-        sections[newSection].titleColor
+        config[newSection].titleColor
       );
     };
 
@@ -91,10 +38,10 @@ const App = () => {
     };
   }, []);
 
-  const renderSections = (sections: Section[]) => {
+  const renderSections = (config: SectionDetails[]) => {
     const sectionElements = [];
 
-    for (const section of sections) {
+    for (const section of config) {
       sectionElements.push(
         <Section
           key={section.title}
@@ -110,7 +57,7 @@ const App = () => {
 
   return (
     <div className="container" id="container" ref={containerRef}>
-      {renderSections(sections)}
+      {renderSections(config)}
     </div>
   );
 };
---
+++ src/Config.tsx
@@ -0,0 +1,54 @@
+import { ReactElement } from 'react';
+
+export interface SectionDetails {
+  title: string;
+  content: ReactElement;
+  id: string;
+  backgroundColor: string;
+  textColor: string;
+  titleColor: string;
+}
+
+const config: SectionDetails[] = [
+  {
+    title: 'daniel',
+    content: <p>test</p>,
+    id: 'about',
+    backgroundColor: '#f2f2f2',
+    textColor: '#121212',
+    titleColor: '#5aa57b',
+  },
+  {
+    title: 'a student',
+    content: (
+      <p>
+        I am a freshman studying computer science at the Paul G. Allen School of
+        Computer Science and Engineering at the University of Washington.
+      </p>
+    ),
+    id: 'education',
+    backgroundColor: '#4b2e83',
+    textColor: '#f2f2f2',
+    titleColor: '#b7a57a',
+  },
+  {
+    title: 'an intern',
+    content: (
+      <p>I am an incoming SDE intern at Amazon for the summer of 2025.</p>
+    ),
+    id: 'experience',
+    backgroundColor: '#232f3e',
+    textColor: '#ff9900',
+    titleColor: '#f2f2f2',
+  },
+  {
+    title: 'a dev',
+    content: <p>test</p>,
+    id: 'projects',
+    backgroundColor: '#ff0000',
+    textColor: '#121212',
+    titleColor: '#33dd33',
+  },
+];
+
+export { config };
